980695a5d73fef9f0aefb74de1c3fdcd56e278c8
Anwar Nunez-Elizalde <anwarnunez@gmail.com>
2020-04-26 20:38:19 +0100

    Some samples

---
+++ repo_template/__init__.py
@@ -0,0 +1,21 @@
+'''
+Example __init__ doc string.
+'''
+
+def example_function():
+    '''Example
+
+    Parameters
+    ----------
+    input1 : float
+    input2 : np.ndarray, (n, p)
+
+    Returns
+    -------
+    ans : bool
+
+    Examples
+    --------
+    >>> data = 'something'
+    '''
+    pass
---
+++ repo_template/template_module.py
@@ -0,0 +1,22 @@
+'''template module
+'''
+
+def function(*arg, **kwargs):
+    '''Description
+
+    Parameters
+    ----------
+    arg1 : int
+        Some input
+    arg2 : float
+        Some other input
+    arg3 : string
+        Yet another input
+    arg4 : 2D np.ndarray, (n, p)
+        An array input
+
+    Returns
+    -------
+    out1 : float
+    '''
+    pass
